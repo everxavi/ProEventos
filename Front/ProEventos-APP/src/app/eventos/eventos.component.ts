4b1f982c14c7631121a19a49ea45bd1f0af69692
Everton Xavier <evertonxavier@hotmail.com>
2021-06-08 01:12:16 -0300

    Comunicação back-front e css

--- Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,21 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any = [
-    {
-      Tema: "Angular 12",
-      Local: "Rio Grande do Norte"
-    },
-
-    {
-      Tema: ".Net Core",
-      Local: "Paraíba"
-    }
-  ]
-
-  constructor() { }
+  public eventos: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
   }
 
+  public getEventos(): void
+  {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      Response => this.eventos = Response,
+      error => console.log(error)
+    );
+    /*
+    this.eventos = [
+      {
+        Tema: "Angular 12",
+        Local: "Rio Grande do Norte"
+      },
+
+      {
+        Tema: ".Net Core",
+        Local: "Paraíba"
+      }
+    ];
+    */
+  }
 }
